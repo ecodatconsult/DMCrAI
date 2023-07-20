d3b0ffa80c0e02946bfb693f4d94637096e34838
aMilles <32961942+aMilles@users.noreply.github.com>
2023-07-20 15:19:40 +0200

    new function to generate bounding boxes with optional blur

---
+++ R/bbox_md_imgs.R
@@ -0,0 +1,40 @@
+#' @export
+#'
+bbox_md_imgs <- function(md_img_info, out.file = tempfile("bbox_img", fileext = ".png"), scale = "x480", safe.mode = TRUE, skip = FALSE){
+
+  imgs <- magick::image_read(md_img_info$file[1]) %>%
+    magick::image_resize(geometry = scale)
+
+  xy <- magick::image_info(imgs)[, c("width", "height")]
+
+  if(!skip){
+    for (bbox in seq(nrow(md_img_info))) {
+      geometry_crop <- ((md_img_info[bbox, c("x_off", "y_off",
+                                             "width", "height")] * as.numeric(c(xy[1], xy[2],
+                                                                                xy[1], xy[2]))))
+      geometry <- with(geometry_crop, {
+        magick::geometry_area(x_off = x_off, y_off = y_off,
+                              width = width, height = height)})
+
+      bbox_image <- magick::image_crop(imgs, geometry)
+
+      # blur sensible data on the fly with safe.mode enabled
+      if(safe.mode & md_img_info$category_word[bbox] %in% c("md_person", "md_vehicle")){
+        bbox_image <- bbox_image %>% magick::image_blur(radius = 30, sigma = 15)
+      }
+
+      bbox_image_border <- magick::image_border(bbox_image, color = "red", geometry = "5x5") %>%
+        magick::image_annotate(text = md_img_info$conf[bbox], size = 20, color = "white", boxcolor = "black")
+
+      imgs <- magick::image_composite(imgs, bbox_image_border,
+                                      offset = geometry)
+    }
+  }
+
+
+  magick::image_write(imgs, out.file)
+
+  return(list(src = normalizePath(out.file),
+       width = xy$width,
+       height = xy$height))
+}
--- R/create_md_folder_structure.R
+++ R/create_md_folder_structure.R
@@ -1,9 +1,9 @@
 #' @export
 #'
 
-create_md_folder_structure <- function(screened_images_files, simple.solution = TRUE){
+create_md_folder_structure <- function(screened_images_files, simple.solution = TRUE, md_dirs = "md_empty"){
   if(simple.solution){
-    for(md_dir in c("md_empty", "md_person", "md_vehicle")){
+    for(md_dir in md_dirs){
       new_dirs <- paste0(unique(dirname(screened_images_files)),
                          .Platform$file.sep,
                          md_dir)
@@ -22,7 +22,7 @@ create_md_folder_structure <- function(screened_images_files, simple.solution = 
     folder_structure_equal <- paste(folder_structure[1,apply(folder_structure, 2, function(x) length(unique(x[!is.na(x)])) == 1)], collapse = "/")
     folder_structure_branch <- unique(folder_structure[,which.min(apply(folder_structure, 2, function(x) length(unique(x[!is.na(x)])) == 1))])
 
-    for(md_dir in c("md_empty", "md_person", "md_vehicle")){
+    for(md_dir in md_dirs){
       dir.create(paste0(folder_structure_equal, "/", md_dir))
 
       for(branch in folder_structure_branch){
