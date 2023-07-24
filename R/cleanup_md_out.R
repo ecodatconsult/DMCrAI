06d6dcaaeaa465387f7d6734c8e3248d134bdc22
aMilles <32961942+aMilles@users.noreply.github.com>
2023-07-24 15:46:43 +0200

    resolved issue with path of new folders added new function to clean md_out.json by removing deprecated paths (not implemented in ShinyDMCrAI yet)

---
+++ R/cleanup_md_out.R
@@ -0,0 +1,20 @@
+#'
+#' @param file Pfade
+#'
+#'
+#' @export
+#'
+
+cleanup_md_out <- function(file){
+  md_out <- rjson::fromJSON(file = file)
+
+  files_exist <-
+  md_out$images  %>%
+    lapply(function(x) file.exists(x$file)) %>%
+    unlist()
+
+  md_out$images <- md_out$images[files_exist]
+
+  rjson::toJSON(md_out) %>%
+    write(file = file)
+}
--- R/create_md_folder_structure.R
+++ R/create_md_folder_structure.R
@@ -1,16 +1,19 @@
+#' Diese Funktion ermittelt das Hauptverzeichnis mehrerer Pfade und legt dort eine neue Unterordner an. In diesen Ordner wird die Ordnerstruktur des Hauptverzeichnis mit allen Unterordnern (ohne den neuen Unterordner) kopiert.
+#'
+#' @param files Pfade
+#'
+#'
 #' @export
 #'
 
-create_md_folder_structure <- function(screened_images_files, simple.solution = TRUE, md_dirs = "md_empty"){
-  if(simple.solution){
-    for(md_dir in md_dirs){
-      new_dirs <- paste0(unique(dirname(screened_images_files)),
-                         .Platform$file.sep,
-                         md_dir)
-      for(new_dir in new_dirs) dir.create(new_dir)
-    }
+create_md_folder_structure <- function(files, md_dirs = "md_empty", slash = "\\"){
+  if(length(unique(basename(dirname(files)))) == 1){
+    empty_dir <- paste0(dirname(dirname(files[1])), slash, "md_empty", slash, basename(dirname(files[1])))
+    dir.create(dirname(empty_dir))
+    dir.create(empty_dir)
+    folder_structure_equal <- dirname(dirname(files[1]))
   }else{
-    folder_structure <- strsplit(screened_images_files, "\\\\")
+    folder_structure <- strsplit(files, paste0(slash, slash))
     folder_depth <- max(unlist(lapply(folder_structure, length)))
     folder_structure <- lapply(folder_structure, function(x){
       x[length(x)] <- NA
@@ -19,19 +22,21 @@ create_md_folder_structure <- function(screened_images_files, simple.solution = 
 
     folder_structure <- do.call(rbind, folder_structure)
 
-    folder_structure_equal <- paste(folder_structure[1,apply(folder_structure, 2, function(x) length(unique(x[!is.na(x)])) == 1)], collapse = "/")
+    folder_structure_equal <- paste(folder_structure[1,apply(folder_structure, 2, function(x) length(unique(x[!is.na(x)])) == 1)], collapse = slash)
     folder_structure_branch <- unique(folder_structure[,which.min(apply(folder_structure, 2, function(x) length(unique(x[!is.na(x)])) == 1))])
 
     for(md_dir in md_dirs){
-      dir.create(paste0(folder_structure_equal, "/", md_dir))
+      dir.create(paste0(folder_structure_equal, slash, md_dir))
 
       for(branch in folder_structure_branch){
-          dir.create(paste0(folder_structure_equal, "/", md_dir, "/", branch))
-          copy_dir(paste0(folder_structure_equal, "/", branch, "/"),
-                   paste0(folder_structure_equal, "/", md_dir ,"/", branch, "/"))
+        dir.create(paste0(folder_structure_equal, slash, md_dir, slash, branch))
+        copy_dir(paste0(folder_structure_equal, slash, branch, slash),
+                 paste0(folder_structure_equal, slash, md_dir ,slash, branch, slash))
       }
     }
-    return(folder_structure_equal)
   }
+
+
+    return(folder_structure_equal)
 }
 
