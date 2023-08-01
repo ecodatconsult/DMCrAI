827122a943bd36cba48986dc3271a6513d4956d6
aMilles <32961942+aMilles@users.noreply.github.com>
2023-08-01 22:37:57 +0200

    added function to extract image metadata

---
+++ R/exif_image_info.R
@@ -0,0 +1,41 @@
+#' Extrahiere Pfad und Erstellungsdatum der Bilder und gib einen data.frame aus
+#'
+#'
+#' @param md_dir character, Verzeichnis in dem rekursiv Bilder gesucht werden
+#' @param info_out character, Verzeichnis in dem die Tabelle zwischengespeichert wird
+#' @param optional_tags character, Weitere exiftool-Tags die in der Tabelle eingetragen werden sollen (z.B: "-Model" um das Kameramodell zu extrahieren)
+#' @examples
+#' #Checks all images in the current directory recursively
+#' exif_image_info(here::here(), optional_tags = "-Model")
+#'
+#' @export
+
+
+
+exif_image_info <- function(md_dir = here::here(), info_out = tempfile("out", tmpdir = here::here(), fileext = ".txt"), optional_tags = NULL){
+
+  tags <- paste0("-filepath -datetimeoriginal ")
+
+  if(!is.null(optional_tags)) tags <- paste0(tags, optional_tags, " ")
+
+  cmd <- paste0('"',system.file("exiftool.exe", package = "DMCrAI"),'"', " -T -r ", tags, '"', md_dir,'"' ," > ", '"', normalizePath(info_out, mustWork = FALSE), '"')
+
+  temp_bat <- tempfile(fileext = ".bat")
+  write(cmd, temp_bat)
+  shell(temp_bat)
+  file.remove(temp_bat)
+
+  img_info <- read.delim(normalizePath(info_out))
+
+  img_info<- img_info[img_info[,2] != "-",]
+
+  names(img_info) <- substr(unlist(strsplit(tags, " ")), 2,35)
+
+  img_info$createdate <- lubridate::as_datetime(img_info$createdate)
+  img_info$deployment <- basename(dirname(img_info$filepath))
+
+  file.remove(info_out)
+
+  return(img_info)
+}
+
