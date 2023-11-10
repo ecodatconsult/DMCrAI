678210a994f6417d1add4140d2e114a98c6aff88
aMilles <32961942+aMilles@users.noreply.github.com>
2023-11-10 15:55:13 +0000

    add upload_md_data.R update

--- R/upload_md_data.R
+++ R/upload_md_data.R
@@ -8,23 +8,23 @@
 
 
 upload_md_data <- function(md_out_events){
-  req_names <- c("deployment", "file", "conf", "category", "x_off", "y_off", "width", "height", "bbox_category","image_category",  "datetimeoriginal")
+  req_names <- c("project_id", "deployment", "file", "conf", "category", "x_off", "y_off", "width", "height", "bbox_category","image_category",  "datetimeoriginal")
 
   if(!all(req_names %in% names(md_out_events))) stop(paste0("missing names in md_out_events (", paste(req_names[!req_names %in% names(md_out_events)], collapse = ", "),  ")!"))
 
 
   con <- dbConnection()
 
-  new_classifications <-
-  md_out_events %>%
+  new_classifications <-  md_out_events %>%
     dplyr::select(dplyr::all_of(req_names)) %>%
-    dplyr::mutate(file_id = digest::digest(file, "md5"), .before = "file")
-
+    dplyr::group_by(file) %>%
+    dplyr::mutate(file_id = digest::digest(file, "md5"), .before = "file") %>%
+    dokyr::ungroup()
   megadetector_classifications_max <- 0
 
   if(RPostgreSQL::dbExistsTable(con,  c("import", "megadetector"))){
     megadetector_classifications <- RPostgreSQL::dbGetQuery(con, glue::glue_sql("SELECT deployment, file_id FROM import.megadetector where deployment in ({deployments*})", deployments = unique(new_classifications$deployment), .con = con))
-    megadetector_classifications_max <- RPostgreSQL::dbGetQuery(con, glue::glue_sql("SELECT max(obs_id) as max FROM import.megadetector", directory_ids = unique(new_classifications$directory_id), .con = con))$max
+    megadetector_classifications_max <- RPostgreSQL::dbGetQuery(con, glue::glue_sql("SELECT max(obs_id) as max FROM import.megadetector", .con = con))$max
 
     new_classifications <-
       new_classifications %>%
@@ -34,7 +34,7 @@ upload_md_data <- function(md_out_events){
   if(nrow(new_classifications) > 0){
     new_classifications <-
     new_classifications %>%
-      dplyr::mutate(obs_id = paste0(deployment, "_", seq(dplyr::n()) + megadetector_classifications_max), .before = "deployment")
+      dplyr::mutate(obs_id = seq(dplyr::n()) + megadetector_classifications_max, .before = "deployment") # obs_id tries to track the total amount of processed images (eventually it may reset if import.megadetector is empty)
 
     RPostgreSQL::dbWriteTable(con, c("import", "megadetector"),
                               new_classifications,
