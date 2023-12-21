ac757443a81f2a7a2453140e8c92ad7469f1456b
aMilles <32961942+aMilles@users.noreply.github.com>
2023-12-21 16:57:43 +0100

    add scale factor as parameter to event images

--- R/ffm2_event_images.R
+++ R/ffm2_event_images.R
@@ -8,7 +8,7 @@ ffm2_event_images <- function(md_out, event_num_selected, scale_factor = .2){
     dplyr::pull(file) %>%
     magick::image_read() |>
     magick::image_join() |>
-    magick::image_resize(geometry = magick::geometry_size_percent(width = 100 * .15))
+    magick::image_resize(geometry = magick::geometry_size_percent(width = 100 * scale_factor))
 }
 
 
